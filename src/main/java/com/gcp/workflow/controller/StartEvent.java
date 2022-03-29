c4be987fe9cadd55bd9eaba0e89bee877c757511
saptaparni.dey <saptaparni.dey@ril.com>
2022-03-29 13:54:16 +0530

    updating code

--- src/main/java/com/gcp/workflow/controller/StartEvent.java
+++ src/main/java/com/gcp/workflow/controller/StartEvent.java
@@ -1,7 +1,10 @@
 package com.gcp.workflow.controller;
 
+import com.gcp.workflow.trigger.TriggerWorkflow;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +15,12 @@ import java.util.concurrent.ExecutionException;
 @RestController
 public class StartEvent {
 
-    @PostMapping("/startevent")
-    public ResponseEntity<String> startEvent(@RequestBody String payload)throws IOException, InterruptedException, ExecutionException {
-//        String resp=triggerWorkflow.triggerWorkflow();
+    @Autowired
+    private TriggerWorkflow triggerWorkflow;
+
+    @GetMapping("/startevent")
+    public ResponseEntity<String> startEvent()throws IOException, InterruptedException, ExecutionException {
+        String resp=triggerWorkflow.triggerWorkflow();
         return new ResponseEntity<>("Workflow Event Started: "+resp, HttpStatus.OK);
     }
 }
